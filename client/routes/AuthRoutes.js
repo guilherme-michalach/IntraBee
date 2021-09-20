85f20387c20db4dbba65dd96d000b5fbefa560dc
Allan <allamfarias954@gmail.com>
2021-09-20 09:34:54 -0300

    Routes

--- client/routes/AuthRoutes.js
+++ client/routes/AuthRoutes.js
@@ -1,2 +1,28 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SignInScreen } from "../screens/SignInScreen";
+import { SignUpScreen } from "../screens/SignUpScreen";
+
+const Stack = createNativeStackNavigator();
+
+export function AuthRoutes() {
+    return(
+        <StackNavigator 
+            initialRouteName = "SignIn"
+        >
+            <Stack.Screen
+                name = "SignIn"
+                component = {SignInScreen}
+                options = {{ headerShown: false }}
+            >
+            </Stack.Screen>
+            <Stack.Screen
+                name = "SignUp"
+                component = {SignUpScreen}
+                options = {}
+            >
+
+            </Stack.Screen>
+        </StackNavigator>
+    );
+}
