2f4e95191087dd05936be7518cf8d78620e97020
Guilherme <guilhermemichalach@gmail.com>
2021-09-23 09:39:23 -0300

    screens

--- client/screens/ConfigScreen.js
+++ client/screens/ConfigScreen.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-export default function ConfigScreen () {}
+export default function ConfigScreen () {
+    // Switch do darkmode
+    // Adm Access
+    // Mudar senha
+    if(!isAdmin) {
+        return (
+
+        )
+    }
+
+    return ()
+}
 
 const styles = StyleSheet.create({
     
