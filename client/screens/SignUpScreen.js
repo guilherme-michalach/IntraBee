0a771e7d8d31c00e8e28a32014cbf270f4fa0712
Guilherme <guilhermemichalach@gmail.com>
2021-10-14 09:08:40 -0300

    sign Up routes with incomplete verification

--- client/screens/SignUpScreen.js
+++ client/screens/SignUpScreen.js
@@ -1,42 +1,105 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from "react-native";
 import colors from "../theme/colors";
 import { EvilIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { api } from "../services/api";
+import { emailRegExp, passwordRegExp } from "../utils/regExpValidations";
+import SplashScreen from "./SplashScreen";
 
 export default function SignUpScreen ({ navigation }) {
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
     const [isPasswordVisible, setIsPasswordVisible] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
 
+    async function handleSignUp () {
+        if(!name) return alert("Por favor insira seu nome!");
+        if(!email) return alert("Por favor insira seu e-mail!");
+
+        if(!emailRegExp.test(email)) return alert("Por favor insira seu e-mail!");
+
+        if(!password) return alert("Por favor insira sua senha!");
+        if(!phone) return alert("Por favor insira seu telefone!");
+
+        setIsLoading(true);
+
+        try {
+            // const createUser = (await api.post("users")).data;
+            const createUser = (await api.post("users", {
+                name,
+                email,
+                phone,
+                password,
+            })).data;
+
+            navigation.push("SignIn")
+
+            // setIsLoading(false);
+        } catch (error) {
+            Alert.alert("Não foi possível realizar o cadastro.");
+            setIsLoading(false);
+        }
+    }
+
+    if (isLoading) {
+        return <SplashScreen />
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>
+            <Text style={styles.name}>
                 IntraBee
             </Text>
-            <Text style={style.label}>
+            <MaterialCommunityIcons style={styles.bee} name="bee" size={100} color="black" />
+            <Text style={styles.title}>
+                Insira os dados abaixo:
+            </Text>
+            <Text style={styles.label}>
                 Nome
             </Text>
-                <TextInput placeholder= "Digite seu nome completo" style={styles.input}/>
+                <TextInput 
+                    placeholder= "Digite seu nome completo" 
+                    style={styles.input}
+                    value={name}
+                    onChangeText={setName}
+                />
             <Text style={styles.label}>
                 E-mail
             </Text> 
-                <TextInput placeholder="Digite seu E-mail" style={styles.input} />
-            <Text style={style.label}>
+                <TextInput 
+                    placeholder="Digite seu E-mail" 
+                    style={styles.input} 
+                    value={email}
+                    onChangeText={setEmail}
+                />
+            <Text style={styles.label}>
                 Telefone
             </Text>
-                <TextInput placeholder= "Digite seu telefone" style={styles.input}/>
+                <TextInput 
+                    placeholder= "Digite seu telefone" 
+                    style={styles.input}
+                    value={phone}
+                    onChangeText={setPhone}
+                />
             <Text style={styles.label}>
                 Senha
             </Text>
             <View style={styles.textHide}>
-                <TextInput secureTextEntry={isPasswordVisible} placeholder="Digite sua senha" style={[styles.input, styles.inputSecure]} />
+                <TextInput 
+                    secureTextEntry={isPasswordVisible} 
+                    placeholder="Digite sua senha" 
+                    style={[styles.input, styles.inputSecure]}
+                    value={password}
+                    onChangeText={setPassword}
+                />
                 <TouchableOpacity style={styles.visibleButton} onPress={() => setIsPasswordVisible(false)}>
                     <EvilIcons name="eye" size={24} color="black" />
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={handleSignUp}>
                 <Text style={styles.buttonText}>
                     Cadastrar
                 </Text>
@@ -52,26 +115,20 @@ const styles = StyleSheet.create({
         backgroundColor: colors.backgroundColor,
         padding: 30
     },
-    label: {
-        fontSize: 18, 
-        marginTop: 10,
-    },
-    input: {
-        fontSize: 16,
-        borderWidth: 1,
-        padding: 10,
-    },
-    button: {
-        backgroundColor: "grey",
-    },
     bee: {
         textAlign: "center"
+    },
+    label: {
+        fontSize: 18,
+        marginTop: 10,
+        color: "black",
+     
     },
     input: {
         fontSize: 16,
         borderBottomWidth: 1,
         borderColor: 'black',
-        padding: 10,   
+        padding: 10,
     },
     button: {
         backgroundColor:  'rgb(192,192,192)',
@@ -86,9 +143,6 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
     title: {
-        color: "black"
-    },
-    logo: {
         textAlign: "center",
         marginTop: 20,
         fontSize: 25,
@@ -104,17 +158,16 @@ const styles = StyleSheet.create({
     textHide: {
         flexDirection: "row",
         alignItems: "center",
-        borderWidth: 1,
+        borderBottomWidth: 1,
+        borderColor:"rgb(47,79,79)",
     },
     inputSecure: {
         flex: 1,
-        borderWidth: 0,
-        borderBottomWidth: 1,
-        borderColor:"rgb(47,79,79)",
+        borderBottomWidth: 0
     },
     visibleButton: {
         width: 40,
         justifyContent: "center",
-        alignItems:"center"
+        alignItems: "center"
     },
 });
