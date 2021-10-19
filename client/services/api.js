d1272cabc937f31c5a3fdfaaaa172cabd4e8ff8e
Guilherme <guilhermemichalach@gmail.com>
2021-10-19 11:51:41 -0300

    video

--- client/services/api.js
+++ client/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://6bae-177-200-213-98.ngrok.io/api"
+    baseURL: "http://5195-177-200-213-98.ngrok.io/api"
 });
--- client/store/actions/types.js
+++ client/store/actions/types.js
@@ -1,2 +1,5 @@
 export const JOIN_CHAT = "JOIN_CHAT";
 export const ADD_STREAM = "ADD_STREAM";
+export const MY_STREAM = "MY_STREAM";
+export const ADD_REMOTE_STREAM = "ADD_REMOTE_STREAM";
+
